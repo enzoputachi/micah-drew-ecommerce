1396650b8402fb41111535277f7b3a9c8aa9a6d2
Nzoputachi <samuelnzoputachi@gmail.com>
2024-07-09 12:51:53 +0100

    Complete stage 5: Build Url Router

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,8 +1,24 @@
+import Error404Screen from "./screens/Error404Screen.js";
 import HomeScreen from "./screens/HomeScreen.js";
+import ProductScreen from "./screens/ProductScreen.js";
+import { parseRequestUrl } from "./utils.js";
 
-const router = () => {
-    const main = document.getElementById("main-container");
-    main.innerHTML = HomeScreen.render();
+//define the route address for each screen
+const routes = {
+    "/": HomeScreen,
+    "/proudct/:id": ProductScreen,
 }
 
-window.addEventListener('load', router)
+//create a router function 
+const router = () => {
+    const request = parseRequestUrl();
+    const parseUrl = (request.resource ? `/${request.resource}`: '/') +
+                    (request.id ? `/${request.id}` : '') +
+                    (request.verb ? `/${request.verb}` :'');
+    const screen = routes[parseUrl]? routes[parseUrl]: Error404Screen;
+    const main = document.getElementById("main-container");
+    main.innerHTML = screen.render();
+}
+
+window.addEventListener('load', router) ;
+window.addEventListener('hashchange', router);
