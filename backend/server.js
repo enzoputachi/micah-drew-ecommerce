5d88b0f4e926cb893fb47fa5aac06871cddf0db2
Nzoputachi <samuelnzoputachi@gmail.com>
2024-07-10 15:22:54 +0100

    Enable Eslinti

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,14 @@
-import express from 'express'
-import products from './data.js';
-import cors from "cors" //to allow transfer of data
+import express from "express";
+import products from "./data.js";
+import cors from "cors"; //to allow transfer of data
 
-const app = express(); 
+const app = express();
 
 app.use(cors());
-app.get('/api/products', (req, res) => {
-    res.send(products);
-})
+app.get("/api/products", (req, res) => {
+  res.send(products);
+});
 
 app.listen(5000, () => {
-    console.log('serve at http://localhost:5000');
+  console.log("serve at http://localhost:5000");
 });
